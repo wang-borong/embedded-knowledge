928022a809e6bf4d1ab0f08bb7830cfc225f7cc9
Jason Wang <wangborong@cdjrlc.com>
2025-12-16 15:46:38 +0800

    Remove unused files

--- pandoc/data/filters/bibexport.lua
+++
@@ -1,5 +0,0 @@
-function Pandoc (doc)
-  doc.meta.references = pandoc.utils.references(doc)
-  doc.meta.bibliography = nil
-  return doc
-end
--- pandoc/data/filters/lineblock.lua
+++
@@ -1,7 +0,0 @@
-function LineBlock(block)
-	return {
-		pandoc.RawBlock("latex", "\\begin{lineblock}"),
-		block,
-		pandoc.RawBlock("latex","\\end{lineblock}")
-	}
-end
